d9461f6a895fdab553b91aeb2e62cbc52c052f5e
WinnieHsu0111 <hsu.win.win@gmail.com>
2026-09-25 03:21:04 -0400

    Remove MAP prior from seirs dmeasure; MAP now lives in efficiency/globalize/mle/R/map_prior.R

--- src/seirs_pomp.c
+++ src/seirs_pomp.c
@@ -345,27 +345,6 @@ void seirs_gill
 
 # define lik  (__lik[0])
 
-// Log-prior for MAP: uniform rectangular prior
-static double seirs_log_prior
-(
- const double *__p,
- const int *__parindex
- ) {
-  double R0_val = Beta / gamma;
-  double p = chi / (gamma + chi);
-  if (R0_val < 1.2 || R0_val > 10.0 ||
-      sigma < 0.2 || sigma > 5.0 ||
-      gamma < 0.2 || gamma > 5.0 ||
-      p < 0.01 || p > 0.7 ||
-      omega < 0.01 || omega > 0.5) {
-    return R_NegInf;
-  }
-  return -log(10.0 - 1.2) - log(5.0 - 0.2) - log(5.0 - 0.2)
-    -log(0.7 - 0.01) - log(0.5 - 0.01);
-}
-
-#define LOGPRIOR (seirs_log_prior(__p,__parindex))
-
 //! Measurement model likelihood (dmeasure).
 void seirs_dmeas
 (
@@ -382,6 +361,5 @@ void seirs_dmeas
  double t
  ) {
   assert(!ISNAN(ll));
-  double lp = ll + LOGPRIOR;
-  lik = (give_log) ? lp : exp(lp);
+  lik = (give_log) ? ll : exp(ll);
 }
